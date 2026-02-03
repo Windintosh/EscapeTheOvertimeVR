493e2101bb9ca6426646ef30634e4b46e22e0889
JungYS24 <ys47497568@gmail.com>
2026-02-03 10:11:40 +0900

    기믹도어 + 심볼패드 테스트중
    도어 베이스를 상속받은 기믹도어 (잠금기능)

---
+++ Source/EscapeTheOvertimeVR/MainClass/Objects/Doors/MainDoors/GimmickDoor.cpp
@@ -0,0 +1,67 @@
+﻿#include "MainClass/Objects/Doors/MainDoors/GimmickDoor.h" 
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundBase.h"
+
+AGimmickDoor::AGimmickDoor()
+{
+	// 기본적으로 잠겨있는 상태로 시작
+	bIsLocked = true;
+}
+
+void AGimmickDoor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AGimmickDoor::Grab_Implementation(USceneComponent* HandController)
+{
+	// 1. 잠겨있는지 확인
+	if (bIsLocked)
+	{
+		// 잠긴 소리 재생
+		if (LockedSound)
+		{
+			UGameplayStatics::PlaySoundAtLocation(this, LockedSound, GetActorLocation());
+		}
+
+		UE_LOG(LogTemp, Warning, TEXT("Door is Locked. Cannot open."));
+
+		// 부모의 Grab(문 열기)을 호출하지 않고 여기서 종료
+		return;
+	}
+
+	// 2. 잠겨있지 않다면 부모 클래스의 원래 기능(문 열기) 실행
+	Super::Grab_Implementation(HandController);
+}
+
+void AGimmickDoor::UnlockDoor(bool bOpenImmediately)
+{
+	if (!bIsLocked) return; // 이미 열려있다면 무시
+
+	bIsLocked = false;
+	UE_LOG(LogTemp, Warning, TEXT("Door Unlocked!"));
+
+	// 잠금 해제 사운드 재생
+	if (UnlockSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, UnlockSound, GetActorLocation());
+	}
+
+	// 옵션: 해제와 동시에 문 열기
+	if (bOpenImmediately)
+	{
+		OpenDoor();
+	}
+}
+
+void AGimmickDoor::LockDoor()
+{
+	if (bIsLocked) return;
+
+	bIsLocked = true;
+
+	// 잠글 때는 문을 닫아줍니다 (열린 채로 잠기면 이상하므로)
+	CloseDoor();
+
+	UE_LOG(LogTemp, Warning, TEXT("Door Locked!"));
+}
---
+++ Source/EscapeTheOvertimeVR/MainClass/Objects/Doors/MainDoors/GimmickDoor.h
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MainClass/Objects/Doors/DoorBase.h"
+#include "GimmickDoor.generated.h"
+
+class USoundBase;
+
+/**
+ * 기본 도어(DoorBase)에 잠금 기능을 추가한 클래스입니다.
+ * bIsLocked가 True일 때 Grab을 시도하면 문이 열리지 않고 잠긴 소리가 재생됩니다.
+ */
+UCLASS()
+class ESCAPETHEOVERTIMEVR_API AGimmickDoor : public ADoorBase
+{
+	GENERATED_BODY()
+
+public:
+	AGimmickDoor();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	// --- 설정 변수 ---
+
+	// 시작 시 잠금 여부 (기본값: True)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gimmick Door")
+	bool bIsLocked;
+
+	// 잠겨있을 때 상호작용 시 재생할 사운드 (철컥)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gimmick Door")
+	USoundBase* LockedSound;
+
+	// 잠금 해제 시 재생할 사운드 (달칵)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gimmick Door")
+	USoundBase* UnlockSound;
+
+	// --- 함수 ---
+
+	// 부모 클래스(DoorBase)의 인터페이스 오버라이드
+	// 잠겨있으면 문 열기 로직을 차단합니다.
+	virtual void Grab_Implementation(USceneComponent* HandController) override;
+
+	// 외부(퍼즐 등)에서 호출하여 잠금을 해제하는 함수
+	// bOpenImmediately: 해제와 동시에 문을 열 것인지 설정
+	UFUNCTION(BlueprintCallable, Category = "Gimmick Door")
+	void UnlockDoor(bool bOpenImmediately = true);
+
+	// 문을 다시 잠그는 함수
+	UFUNCTION(BlueprintCallable, Category = "Gimmick Door")
+	void LockDoor();
+};
