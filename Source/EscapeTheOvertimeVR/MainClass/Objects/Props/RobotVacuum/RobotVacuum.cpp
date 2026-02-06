308d034e75d0a0ecb087b482e355d54ecda2337a
Jae-Hong Park <48243825+Windintosh@users.noreply.github.com>
2026-02-06 16:23:57 +0900

    Update RobotVacuum.cpp
    RobotVacuum is now stopped by TQProjectile

--- Source/EscapeTheOvertimeVR/MainClass/Objects/Props/RobotVacuum/RobotVacuum.cpp
+++ Source/EscapeTheOvertimeVR/MainClass/Objects/Props/RobotVacuum/RobotVacuum.cpp
@@ -92,13 +92,15 @@ void ARobotVacuum::Release_Implementation(FVector ThrowVelocity)
 
 void ARobotVacuum::OnPropOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	ATQAmmo* Ammo = Cast<ATQAmmo>(OtherActor);
-	if (Ammo)
+    AActor* Ammo = Cast<AActor>(OtherActor);
+	if (Ammo->GetClass()->GetName().Contains(TEXT("Projectile")))
 	{
 		ActivateProp(OtherActor);
+        StopRVMovement();
         return;
 	}
     //StartRVMovement();
+    //StopRVMovement();
 }
 
 void ARobotVacuum::ActivateProp(AActor* Activator)
